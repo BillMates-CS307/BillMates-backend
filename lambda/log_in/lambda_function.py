ea794ccce970e3bf75a76f4c22e4bf0df7db23db
Ben Lilley <benlilleydev@gmail.com>
2023-02-17 14:09:24 -0500

    Updated Log_In with Fake Database Tests

--- lambda/log_in/lambda_function.py
+++ lambda/log_in/lambda_function.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import email
 
 def grab_json_from_url(url: str):
     resp = requests.get(url)
@@ -8,16 +9,20 @@ def grab_json_from_url(url: str):
 def lambda_handler(event, context):
     
     # Parameters
-    payload = event['queryStringParameters']
-    
+    payload = json.loads(event['body'])
     # Verify token is correct before running more commands
     token = event['headers']['token']
     response = {"echo" : payload, "response": {}}
     
     verification = grab_json_from_url("https://cdv3yr23omakr4fkrtqgbpeela0zejwf.lambda-url.us-east-2.on.aws/?token=" + token)
-    response['response']["verified"] = verification['response']
+    response['response']["token_verified"] = verification['response']
     
-    
+    # Fake database for testings
+    if verification['response']:
+        usr, pw = payload['email'], payload['password']
+        with open("pho_db.json") as f:
+            obj = json.load(f)
+            response['response']['success'] = usr in obj['ledger'] and obj['ledger'][usr] == pw
     return {
             'statusCode': 200,
             'headers': {
--- lambda/testing/client/url.py
+++ lambda/testing/client/url.py
@@ -4,11 +4,12 @@ import json
 import requests
 
 def grab_json_from_url(url: str) -> json:
-    resp = requests.get(url)
+    headers = {'token': 'zpdkwA.2_kLU@zg'}
+    resp = requests.get(url, headers=headers)
     return resp.json()
 
 def main():
-    my_json = grab_json_from_url("https://jwfjuifdunib5gmornhrs4nm4a0pitnm.lambda-url.us-east-2.on.aws/?token=zpdkwA.2_kLU@zg")
-    print("Works")
+    my_json = grab_json_from_url('https://jwfjuifdunib5gmornhrs4nm4a0pitnm.lambda-url.us-east-2.on.aws/?user=bhlilley&pass=my_pass')
+    print(my_json)
 
 main()
