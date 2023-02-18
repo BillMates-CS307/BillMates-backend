c4b9965314cb48efb9c910c373ee658146c257b7
Ben Lilley <benlilleydev@gmail.com>
2023-02-17 23:30:38 -0500

    Created layout for common codebase
    Cleaned up log_in function. Added common codebase called bundle that is added to each function for commonly used functions. Look at log_in for how to format/import it. Anytime new code is added to `bundle/` make sure to update it in each function.

--- lambda/testing/client/url.py
+++ lambda/testing/client/url.py
@@ -2,14 +2,23 @@
 
 import json
 import requests
+from enum import Enum
+
+class Action(Enum):
+    TEST=1
+    BRO=2
 
 def grab_json_from_url(url: str) -> json:
     headers = {'token': 'zpdkwA.2_kLU@zg'}
-    resp = requests.get(url, headers=headers)
+    body = {'email':'bhlilley@purdue.edu', 'password':'my_pass'}
+    resp = requests.post(url, headers=headers, json=body)
     return resp.json()
 
+def bro(action: Action):
+    return action 
+
 def main():
-    my_json = grab_json_from_url('https://jwfjuifdunib5gmornhrs4nm4a0pitnm.lambda-url.us-east-2.on.aws/?user=bhlilley&pass=my_pass')
+    my_json = grab_json_from_url('https://jwfjuifdunib5gmornhrs4nm4a0pitnm.lambda-url.us-east-2.on.aws/')
     print(my_json)
 
 main()
