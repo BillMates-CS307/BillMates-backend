8191335fb8de2c5e7debfecb2ddc0ea79c7f2d3d
rrittner1 <rrittner@purdue.edu>
2023-02-22 17:59:59 -0500

    Sign_up works with database

--- lambda/sign_up/lambda_function.py
+++ lambda/sign_up/lambda_function.py
@@ -1,51 +1,51 @@
 import os
 import json
 import requests
+from pymongo import MongoClient
+import bundle.mongo as mongo
+import bundle.api as api
 
-def access_mongodb():
-    mongodb_uri = os.environ['MONGODB_URI']
-    client = pymongo.MongoClient(mongodb_uri)
-    db = client.billmates
-    return db
 
 def grab_json_from_url(url: str):
     resp = requests.get(url)
     return resp.json()
 
+
 def lambda_handler(event, context):
-    db = access_mongodb()
-    
     token = event['headers']['token']
-    response = {"echo" : "None", "response": {}}
-    
+    response = {"echo": "None", "response": {}}
+
     # token verification
-    verification = grab_json_from_url("https://cdv3yr23omakr4fkrtqgbpeela0zejwf.lambda-url.us-east-2.on.aws/?token=" + token)
-    response['response']["verified"] = verification['response']
-    
-    if verification['response']:
+    response['response']["verified"] = api.check_token(token)
+
+    if response['response']["verified"]:
+        db = mongo.get_database()
+
         # retrieving parameters
         parameters = json.loads(event['body'])
-        username = parameters['email']
+        email = parameters['email']
         password = parameters['password']
-        
-        # Query database to determine if username available
-        # If available add username and password to database and return success
-        # If not return failure
-        
-        with open("pho_db.json") as f:
-            obj = json.load(f)
-            if username in obj['ledger']:
-                response['details'] = 'failure'
-            else:
-                response['details'] = 'success'
-                # add username and passsword to database
-        
+        name = parameters['name']
+
+        users = db['users']
+        user = mongo.query_user({'email': email})
+        if user is None:
+            new_user = {
+                'email': email,
+                'password': password,
+                'name': name
+            }
+            users.insert_one(new_user)
+            response['sign_up_success'] = True
+        else:
+            response['sign_up_success'] = False
+
         return {
-                'statusCode': 200,
-                'headers': {
-                    'Access-Control-Allow-Headers': 'Content-Type',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-                },
-                'body': json.dumps(response)
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': json.dumps(response)
         }
