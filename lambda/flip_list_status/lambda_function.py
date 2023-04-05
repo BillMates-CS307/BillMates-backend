0b1b811b54c9d25015293863d671a423941764b8
Ben Lilley <benlilleydev@gmail.com>
2023-04-05 12:43:59 -0400

    Added flip_list_status API

---
+++ lambda/flip_list_status/lambda_function.py
@@ -0,0 +1,27 @@
+import json
+import bundle.api as api
+from pymongo import MongoClient
+import bundle.mongo as mongo
+from bson.objectid import ObjectId
+
+def lambda_handler(event, context):
+    # Parameters
+    payload = json.loads(event['body'])
+    
+    # Build reponse JSON
+    response = {}
+
+    # Verify token is correct before running more commands
+    token = event['headers']['token']
+    response["token_success"] = api.check_token(token)
+    response["change_success"] = False
+    if response['token_success']:
+        db = mongo.get_database()
+        shop = mongo.query_table("shoppinglists", {'_id' : ObjectId(payload['list_id'])}, db)
+        if shop == None:
+            return api.build_capsule(response)
+            
+        response["change_success"] = True
+        db['shoppinglists'].update_one({"_id" : ObjectId(payload['list_id'])}, {"$set": {"isActive" : not shop["isActive"]}})
+
+    return api.build_capsule(response)
---
+++ lambda/testing/client/flipliststatus.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import json
+import requests
+
+def grab_json_from_url(url: str) -> json:
+    headers = {'token': 'zpdkwA.2_kLU@zg'}
+    body = {'list_id' : '642d9577259e83029d17a2ae'}
+    resp = requests.post(url, headers=headers, json=body)
+    return resp.json()
+
+def main():
+    my_json = grab_json_from_url('https://gdmqnuwrzqsja2ps3zc2z2y4km0ejbyu.lambda-url.us-east-2.on.aws/')
+    print(my_json)
+
+main()
