bbe23e87b0963e85660e148d57613925fe938d8b
Ben Lilley <benlilleydev@gmail.com>
2023-04-05 16:37:51 -0400

    "Final" changes to update_shopping_lists

--- lambda/testing/client/updatelist.py
+++ lambda/testing/client/updatelist.py
@@ -5,7 +5,7 @@ import requests
 
 def grab_json_from_url(url: str) -> json:
     headers = {'token': 'zpdkwA.2_kLU@zg'}
-    body = {'list_id' : '642d9577259e83029d17a2ae', 'item_name': 'new_item2', 'remove_item': True}
+    body = {'list_id' : '642d9577259e83029d17a2ae', 'item_name': 'item_lol', 'remove_item': False}
     resp = requests.post(url, headers=headers, json=body)
     return resp.json()
 
--- lambda/update_shopping_list/lambda_function.py
+++ lambda/update_shopping_list/lambda_function.py
@@ -18,21 +18,17 @@ def lambda_handler(event, context):
     if response['token_success']:
         db = mongo.get_database()
         shop = mongo.query_table("shoppinglists", {'_id' : ObjectId(payload['list_id'])}, db)
+        name = payload['item_name']
         if shop == None or not shop['isActive']:
             return api.build_capsule(response)
         response["change_success"] = True
-        idx = -1
         items = shop['items']
-        for i in range(len(items)):
-            if items[i]['name'] == payload['item_name']:
-                idx = i
-                break
-        if idx == -1 and payload['remove_item'] or idx != -1 and not payload['remove_item']:
+        if name not in items and payload['remove_item'] or name in items and not payload['remove_item']:
             return api.build_capsule(response)
         if payload['remove_item']:
-            items.pop(idx)
+            items.remove(name)
         else:
-            items.append({'name': payload['item_name'], 'cost':0, 'quantity':0})
+            items.append(name)
         db['shoppinglists'].update_one({'_id' : ObjectId(payload['list_id'])}, {'$set' : {'items' : items}})
         
     return api.build_capsule(response)
