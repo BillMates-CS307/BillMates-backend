b844e69f8c11dae110d686717da7bcf366576d97
rrittner1 <rrittner@purdue.edu>
2023-04-10 14:51:36 -0400

    Created get_user_calendar

---
+++ lambda/get_user_calendar/lambda_function.py
@@ -0,0 +1,71 @@
+import json
+import bundle.api as api
+from pymongo import MongoClient
+import bundle.mongo as mongo
+from bson.objectid import ObjectId
+import datetime
+
+def lambda_handler(event, context):
+    # Parameters
+    payload = json.loads(event['body'])
+    email = payload['email']
+    
+    # Build reponse JSON
+    response = {}
+
+    # Verify token is correct before running more commands
+    token = event['headers']['token']
+    response["token_success"] = api.check_token(token)
+    response["get_success"] = False
+    if response['token_success']:
+        db = mongo.get_database()
+        user = mongo.query_table("users", {'email' : email}, db)
+        if user == None:
+            return api.build_capsule(response)
+        response['get_success'] = True
+        groups = user['groups']
+        
+        events = []
+        recurs = []
+        for g in groups:
+            cal = db['calendars'].find_one({'group_id': g})
+            if cal is None :
+                continue
+            for e in cal['events']:
+                e['group_id'] = str(g);
+                events.append(e)
+            for r in cal['recurring_expenses']:
+                r['group_id'] = str(g)
+                recurs.append(r)
+        
+        event_list = []
+        for e in events:
+            o = {}
+            o['group_id'] = e['group_id']
+            o['creator'] = e['creator']
+            o['name'] = e['name']
+            o['description'] = e['description']
+            o['date'] = e['date']
+            o['time'] = e['time']
+            o['total'] = None
+            o['expense'] = None
+            o['frequency'] = None
+            o['location'] = e['location']
+            event_list.append(o)
+        for r in recurs:
+            o = {}
+            o['group_id'] = e['group_id']
+            o['creator'] = r['creator']
+            o['name'] = r['name']
+            o['description'] = r['description']
+            o['date'] = r['date']
+            o['time'] = r['time']
+            o['total'] = r['total']
+            o['expense'] = r['expense']
+            o['frequency'] = r['frequency']
+            o['location'] = None
+            event_list.append(o)
+        response['events'] = event_list
+        
+
+    return api.build_capsule(response)
---
+++ lambda/testing/client/getusercalendar.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import json
+import requests
+
+def grab_json_from_url(url: str) -> json:
+    headers = {'token': 'zpdkwA.2_kLU@zg'}
+    body = {'email' : "rdrittner@gmail.com"}
+    resp = requests.post(url, headers=headers, json=body)
+    return resp.json()
+
+def main():
+    my_json = grab_json_from_url('https://6b7hetv76hhwtm6ewy2ucl5sry0knann.lambda-url.us-east-2.on.aws/')
+    print(my_json)
+
+main()
